4d636edbda5da1da382987cfdd54b0f01a004b7f
Petros Angelatos <petrosagg@gmail.com>
2015-10-06 02:26:59 +0100

    increase sleep after loading kernel module
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ ! -c /dev/fb1 ]; then
   modprobe spi-bcm2708
   modprobe fbtft_device name=pitft verbose=0 rotate=0
 
-  sleep 1
+  sleep 2
 
   mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ')
 fi
