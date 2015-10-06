5e5c02c07b61fd5ef40ce0909caf9e9d667c5170
Petros Angelatos <petrosagg@gmail.com>
2015-10-06 11:38:28 +0100

    only load pitft module on raspberrypis
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ ! -c /dev/fb1 ]; then
-  echo "loading piTFT kernel module"
-  modprobe spi-bcm2708
-  modprobe fbtft_device name=pitft verbose=0 rotate=0
+if [ "$DEVICE" == 'raspberrypi' ]; then
+	if [ ! -c /dev/fb1 ]; then
+	  echo "loading piTFT kernel module"
+	  modprobe spi-bcm2708
+	  modprobe fbtft_device name=pitft verbose=0 rotate=0
 
-  sleep 2
+	  sleep 2
 
-  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ')
+	  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ')
+	fi
 fi
 
 exec python /usr/src/app/show.py
